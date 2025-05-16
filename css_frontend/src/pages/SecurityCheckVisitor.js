58421bf2d027e6bba446f4f5b6c94b2f7bfb7c3b
Moath66 <mohamed-a2@graduate.utm.my>
2025-05-16 15:01:47 +0800

    .

--- css_frontend/src/pages/SecurityCheckVisitor.js
+++ css_frontend/src/pages/SecurityCheckVisitor.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../../styles/SecurityCheckVisitor.css";
+import "../styles/SecurityCheckVisitor.css";
 import axios from "axios";
 
 const SecurityCheckVisitor = () => {
