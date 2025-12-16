55ebebd17fef155cd439fe907022d9533fa2abca
Moath_Morsy.div <116039955+Moath66@users.noreply.github.com>
2025-12-16 18:29:17 +0300

    Add authentication middleware to user routes

--- css_backend/api_routes/userRoutes.js
+++ css_backend/api_routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const auth = require("../middlewares/authMiddleware");
 
 // Authentication Routes
 router.post("/register", userController.createUser);  // Register a user
