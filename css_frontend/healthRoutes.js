f794681a8a4040e9fdf595c789798ff36480a14c
Moath_Morsy.div <116039955+Moath66@users.noreply.github.com>
2025-12-16 18:54:38 +0300

    Remove healthRoutes.js file

--- css_frontend/healthRoutes.js
+++ css_frontend/healthRoutes.js
@@ -1,13 +1,1 @@
-// api_routes/healthRoutes.js
-const express = require("express");
-const router = express.Router();
 
-router.get("/health", (req, res) => {
-  res.status(200).json({
-    ok: true,
-    status: "healthy",
-    timestamp: new Date().toISOString(),
-  });
-});
-
-module.exports = router;
